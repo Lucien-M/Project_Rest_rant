2f4689c6ba9f433e730a43c459d3e3c7dfda3624
Lucien-M <mail@gmail.com>
2022-10-10 18:28:45 -0400

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,10 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+  res.send('POST /places')
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -20,6 +24,8 @@ router.get('/', (req, res) => {
       }]
     res.render('places/index', {places})
 })
+
+
 
 
 
