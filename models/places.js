1e58ff7e5f29c2aa4cd5787c87dc9a3d31c0c498
Lucien-M <mail@gmail.com>
2022-10-31 11:03:25 -0400

    Helper Method added and used

--- models/places.js
+++ models/places.js
@@ -1,26 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   pic: String,
   cuisines: { type: String, required: true },
-  city: { type: String, default: 'Anytown' },
-  state: { type: String, default: 'USA' },
+  city: { type: String, default: "Anytown" },
+  state: { type: String, default: "USA" },
   founded: Number
-})
+});
 
-module.exports = mongoose.model('Place', placeSchema)
+placeSchema.methods.showEstablished = function () {
+  return `${this.name} has been serving ${this.city} since ${this.founded}.`;
+};
 
-  // [{
-  //   name: 'H-Thai-ML',
-  //   city: 'Seattle',
-  //   state: 'WA',
-  //   cuisines: 'Thai, Pan-Asian',
-  //   pic: '/images/meal_tab.jpg'
-  // }, {
-  //   name: 'Coding Cat Cafe',
-  //   city: 'Phoenix',
-  //   state: 'AZ',
-  //   cuisines: 'Coffee, Bakery',
-  //   pic: '/images/meal_dis.jpg'
-  // }]
+module.exports = mongoose.model("Place", placeSchema);
+
+// [{
+//   name: 'H-Thai-ML',
+//   city: 'Seattle',
+//   state: 'WA',
+//   cuisines: 'Thai, Pan-Asian',
+//   pic: '/images/meal_tab.jpg'
+// }, {
+//   name: 'Coding Cat Cafe',
+//   city: 'Phoenix',
+//   state: 'AZ',
+//   cuisines: 'Coffee, Bakery',
+//   pic: '/images/meal_dis.jpg'
+// }]
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,51 +1,43 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show (data) {
+function show(data) {
   return (
     <Def>
-    <main>
-      <div className="row">
-        <div className="col-sm-6">
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            {/*<h1>{data.place.name}</h1>*/}
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
             <h1>{data.place.name}</h1>
-          <img src={data.place.pic} alt={data.place.name} />
-          <h3>
-            Located in {data.place.city}, {data.place.state}
-          </h3>
-        </div>
-        <div className="col-sm-6">
-          <h2>Ratings</h2>
-          <p>currently unrated</p>
-          <h2>
-            Description
-          </h2>
-          {/*<h3>
-            {data.place.showEstablished()}
-  </h3>*/}
-          <h4>
-            Serving {data.place.cuisines}
-          </h4>
-          <div>
-              <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                  Edit
-              </a>       
-              <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                  <button type="submit" className="btn btn-danger">
-                      Delete
-                  </button>
-              </form>    
+            <h2>Ratings</h2>
+            <p>Not Rated</p>
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+            <div>
+              <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                Edit
+              </a>
+              <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                <button type="submit" className="btn btn-danger">
+                  Delete
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row">
+        <div className="row">
           <h2>Comments</h2>
-          <a href={`/places/${data.id}/rant`}> 
-                  Add a comment
-          </a> 
-     </div>
-    </main>
-  </Def>
-        
-  )
+          <a href={`/places/${data.id}/rant`}>No comments yet!</a>
+        </div>
+      </main>
+    </Def>
+  );
 }
-module.exports = show
+module.exports = show;
